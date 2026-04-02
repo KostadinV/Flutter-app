0ed68c2cb9c30922843277b4cd15bdb0e1ffc2d5
Kostadin Teodosiev <kostadin.teodosiev@gmail.com>
2026-04-02 16:06:53 +0300

    Fix indentation

--- lib/main.dart
+++ lib/main.dart
@@ -34,37 +34,34 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _notificationsEnabled = true;
   double _notificationThreshold = 10;
 
-  final List<String> _notifications = [
-    'Notification 1',
-    'Notification 2',
-  ];
+  final List<String> _notifications = ['Notification 1', 'Notification 2'];
 
   void _incrementCounter() {
     setState(() {
       _counter++;
-      if (_notificationsEnabled && _counter % _notificationThreshold.toInt() == 0  && _counter != 0) {
+      if (_notificationsEnabled &&
+          _counter % _notificationThreshold.toInt() == 0 &&
+          _counter != 0) {
         _notifications.insert(0, 'You made $_counter taps!');
       }
     });
   }
 
   void _resetCounter() {
-  setState(() {
-    _counter = 0;
-  });
-  
-  // Optional: Task for the students
-  ScaffoldMessenger.of(context).showSnackBar(
-    const SnackBar(content: Text('Counter reset to zero')),
-  );
-  _notifications.insert(0, 'You reset the counter to 0');
-}
+    setState(() {
+      _counter = 0;
+    });
+
+    // Optional: Task for the students
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('Counter reset to zero')));
+    _notifications.insert(0, 'You reset the counter to 0');
+  }
 
   @override
   Widget build(BuildContext context) {
-
     final List<Widget> pages = [
-
       Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -85,124 +82,141 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
 
       Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemCount: _notifications.length,
-            itemBuilder: (context, index) {
-              return Dismissible(
-                key: Key(_notifications[index] + index.toString()),
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemCount: _notifications.length,
+          itemBuilder: (context, index) {
+            return Dismissible(
+              key: Key(_notifications[index] + index.toString()),
 
-                onDismissed: (direction){
-                  if (direction == DismissDirection.startToEnd){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Notification Archived")),
-                    );
-                    setState(() {
+              onDismissed: (direction) {
+                if (direction == DismissDirection.startToEnd) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Notification Archived")),
+                  );
+                  setState(() {
                     _notifications.removeAt(index);
                   });
-                  }else {
-                    setState((){
-                      _notifications.removeAt(index);
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Notification Deleted")),
-                    );
-                  }
-                },
-                secondaryBackground: Container(
-                  color: Colors.red,
-                  alignment: Alignment.centerRight,
-                  padding: const EdgeInsets.only(right: 20),
-                  child: const Icon(Icons.delete, color: Colors.white),
-                ),
-                background: Container(
-                  color: Colors.green,
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(left: 20),
-                  child: const Icon(Icons.archive, color: Colors.white),
-                ),
+                } else {
+                  setState(() {
+                    _notifications.removeAt(index);
+                  });
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Notification Deleted")),
+                  );
+                }
+              },
+              secondaryBackground: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.only(right: 20),
+                child: const Icon(Icons.delete, color: Colors.white),
+              ),
+              background: Container(
+                color: Colors.green,
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 20),
+                child: const Icon(Icons.archive, color: Colors.white),
+              ),
               child: Card(
                 child: ListTile(
                   leading: Icon(Icons.notifications_sharp),
                   title: Text(_notifications[index]),
                   subtitle: Text('This is a dynamic notification'),
                   trailing: IconButton(
-                    icon: const Icon(Icons.delete_outline, color: Colors.redAccent),
+                    icon: const Icon(
+                      Icons.delete_outline,
+                      color: Colors.redAccent,
+                    ),
                     onPressed: () {
-                    setState(() {
-                      _notifications.removeAt(index);
-                    });
-                  }, 
+                      setState(() {
+                        _notifications.removeAt(index);
+                      });
+                    },
+                  ),
                 ),
-                ),
-              ),);
-            },
-            ),
-            ),
-            ListView(
-    children: [
-      const Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Text("Notifications", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+              ),
+            );
+          },
+        ),
       ),
-      SwitchListTile(
-        title: const Text("Enable Tap Notifications", style: TextStyle(fontWeight: FontWeight.bold)),
-        subtitle: const Text("Get notified when hitting milestones"),
-        value: _notificationsEnabled,
-        secondary: const Icon(Icons.vibration),
-        onChanged: (bool value) {
-          setState(() {
-            _notificationsEnabled = value;
-          });
-        },
-      ),
-      const Divider(),
-      ListTile(
-  title: const Text("Hitting milestone", style: TextStyle(fontWeight: FontWeight.bold)),
-  subtitle: Text("Every ${_notificationThreshold.toInt()} taps"),
-  leading: const Icon(Icons.emoji_events),
-  onTap: () {
-    // Create a controller to handle the text inside the dialog
-    TextEditingController customController = TextEditingController(text: _notificationThreshold.toInt().toString());
-
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Set Milestone"),
-          content: TextField(
-            controller: customController,
-            keyboardType: TextInputType.number,
-            autofocus: true, // Automatically opens the keyboard
-            decoration: const InputDecoration(
-              labelText: "Number of taps",
-              suffixText: "taps",
-              border: OutlineInputBorder(),
+      ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              "Notifications",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
           ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text("Cancel"),
+          SwitchListTile(
+            title: const Text(
+              "Enable Tap Notifications",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            ElevatedButton(
-              onPressed: () {
-                double? newValue = double.tryParse(customController.text);
-                if (newValue != null && newValue > 0) {
-                  setState(() => _notificationThreshold = newValue);
-                  Navigator.pop(context);
-                }
-              },
-              child: const Text("Save"),
+            subtitle: const Text("Get notified when hitting milestones"),
+            value: _notificationsEnabled,
+            secondary: const Icon(Icons.vibration),
+            onChanged: (bool value) {
+              setState(() {
+                _notificationsEnabled = value;
+              });
+            },
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text(
+              "Hitting milestone",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-          ],
-        );
-      },
-    );
-  },
-),
-    ],
-  ),
+            subtitle: Text("Every ${_notificationThreshold.toInt()} taps"),
+            leading: const Icon(Icons.emoji_events),
+            onTap: () {
+              // Create a controller to handle the text inside the dialog
+              TextEditingController customController = TextEditingController(
+                text: _notificationThreshold.toInt().toString(),
+              );
+
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("Set Milestone"),
+                    content: TextField(
+                      controller: customController,
+                      keyboardType: TextInputType.number,
+                      autofocus: true, // Automatically opens the keyboard
+                      decoration: const InputDecoration(
+                        labelText: "Number of taps",
+                        suffixText: "taps",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text("Cancel"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          double? newValue = double.tryParse(
+                            customController.text,
+                          );
+                          if (newValue != null && newValue > 0) {
+                            setState(() => _notificationThreshold = newValue);
+                            Navigator.pop(context);
+                          }
+                        },
+                        child: const Text("Save"),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
     ];
 
     return Scaffold(
@@ -214,12 +228,12 @@ class _MyHomePageState extends State<MyHomePage> {
       body: pages[_selectedIndex],
 
       floatingActionButton: _selectedIndex == 0
-      ? FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      )
-      : null,
+          ? FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            )
+          : null,
 
       bottomNavigationBar: NavigationBar(
         selectedIndex: _selectedIndex,
@@ -249,7 +263,6 @@ class _MyHomePageState extends State<MyHomePage> {
             label: 'Settings',
           ),
         ],
-        
       ),
     );
   }
